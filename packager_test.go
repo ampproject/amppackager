ampproject/amppackager

commit 3c3f199694da5ecd19d230514bc47e4302ce20d2

Rewrite fetch URLs to fetch through the AMP CDN.
This enables server-side transformations for the URLs. (To be replaced
by an AMP Transformation API later.) The URL rewrite should match the
AMP CDN's implementation.

Also made a couple tweaks to docs and errors (adding stack traces).

Change-Id: Id454c3ad7df88d65528be85037b35fd80b5f7d81

packager.go
@@ -121,22 +121,22 @@ func urlsMatch(fetchURL *url.URL, signURL *url.URL, set URLSet) bool {
 		(!set.SamePath || fetchURL.RequestURI() == signURL.RequestURI())
 }
 
-// Returns parsed sign URL and whether to fail on stateful headers.
-func parseURLs(fetch string, sign string, urlSets []URLSet) (*url.URL, bool, *HTTPError) {
+// Returns parsed URLs and whether to fail on stateful headers.
+func parseURLs(fetch string, sign string, urlSets []URLSet) (*url.URL, *url.URL, bool, *HTTPError) {
 	fetchURL, err := parseURL(fetch, "fetch")
 	if err != nil {
-		return nil, false, err
+		return nil, nil, false, err
 	}
 	signURL, err := parseURL(sign, "sign")
 	if err != nil {
-		return nil, false, err
+		return nil, nil, false, err
 	}
 	for _, set := range urlSets {
 		if urlsMatch(fetchURL, signURL, set) {
-			return signURL, set.Fetch.ErrorOnStatefulHeaders, nil
+			return fetchURL, signURL, set.Fetch.ErrorOnStatefulHeaders, nil
 		}
 	}
-	return nil, false, NewHTTPError(http.StatusBadRequest, "fetch/sign URLs do not match config")
+	return nil, nil, false, NewHTTPError(http.StatusBadRequest, "fetch/sign URLs do not match config")
 }
 
 func validateFetch(req *http.Request, resp *http.Response) *HTTPError {
@@ -145,7 +145,10 @@ func validateFetch(req *http.Request, resp *http.Response) *HTTPError {
 	}
 	// Validate response is publicly-cacheable, per
 	// https://tools.ietf.org/html/draft-yasskin-http-origin-signed-responses-03#section-6.1.
-	nonCachableReasons, _, err := cachecontrol.CachableResponse(req, resp, cachecontrol.Options{PrivateCache: false})
+	// TODO(twifkak): Set {PrivateCache: false} after we switch from
+	// fetching through the AMP CDN to fetching directly and using the
+	// transformer API.
+	nonCachableReasons, _, err := cachecontrol.CachableResponse(req, resp, cachecontrol.Options{PrivateCache: true})
 	if err != nil {
 		return NewHTTPError(http.StatusBadGateway, "Error parsing cache headers: ", err)
 	}
@@ -190,10 +193,15 @@ func NewPackager(cert *x509.Certificate, key crypto.PrivateKey, packagerBase str
 	return &Packager{cert, key, validityURL, &client, baseURL, urlSets}, nil
 }
 
-func (this Packager) fetchURL(fetch string) (*http.Request, *http.Response, *HTTPError) {
-	log.Printf("Fetching URL: %q\n", fetch)
+func (this Packager) fetchURL(url *url.URL) (*http.Request, *http.Response, *HTTPError) {
+	ampURL := "https://cdn.ampproject.org/c/"
+	if url.Scheme == "https" {
+		ampURL += "s/"
+	}
+	ampURL += url.Host + url.RequestURI()
+	log.Printf("Fetching URL: %q\n", ampURL)
 	// TODO(twifkak): Translate into AMP CDN URL, until transform API is available.
-	req, err := http.NewRequest(http.MethodGet, fetch, nil)
+	req, err := http.NewRequest(http.MethodGet, ampURL, nil)
 	req.Header.Set("User-Agent", userAgent)
 	// TODO(twifkak): Should we add 'Accept-Charset: utf-8'? The AMP Transformer API requires utf-8.
 	if err != nil {
@@ -220,13 +228,13 @@ func (this Packager) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	// TODO(twifkak): See if there are any other validations or sanitizations that need adding.
 	fetch := req.FormValue("fetch")
 	sign := req.FormValue("sign")
-	signURL, errorOnStatefulHeaders, httpErr := parseURLs(fetch, sign, this.urlSets)
+	fetchURL, signURL, errorOnStatefulHeaders, httpErr := parseURLs(fetch, sign, this.urlSets)
 	if httpErr != nil {
 		httpErr.LogAndRespond(resp)
 		return
 	}
 
-	fetchReq, fetchResp, httpErr := this.fetchURL(fetch)
+	fetchReq, fetchResp, httpErr := this.fetchURL(fetchURL)
 	if httpErr != nil {
 		httpErr.LogAndRespond(resp)
 		return
@@ -251,12 +259,13 @@ func (this Packager) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 		}
 		fetchResp.Header.Del(header)
 	}
+
 	fetchBody, err := ioutil.ReadAll(io.LimitReader(fetchResp.Body, maxBodyLength))
 	if err != nil {
-		log.Println("Error reading body:", err)
-		http.Error(resp, "502 bad gateway", http.StatusBadGateway)
+		NewHTTPError(http.StatusBadGateway, "Error reading body: ", err).LogAndRespond(resp)
 		return
 	}
+
 	exchange, err := signedexchange.NewExchange(signURL, http.Header{}, fetchResp.StatusCode, fetchResp.Header, fetchBody, miRecordSize)
 	if err != nil {
 		NewHTTPError(http.StatusInternalServerError, "Error building exchange: ", err).LogAndRespond(resp)

.gitignore
@@ -1,2 +1,3 @@
 amppkg.toml
 amppkg
+test_cache

amppkg.example.toml
@@ -59,7 +59,7 @@ GoogleAPIKey = "asdfghjkl"
 #
 # This config doesn't let you specify an exact URL-to-URL mapping between fetch
 # and sign. If an attacker gains direct access to this packager, they could ask
-# it to fetch http://www.corp.example.com/foo.html and sign it as
+# it to fetch https://www.example.com/foo.html and sign it as
 # https://example.com/bar.html.
 #
 # Instead, it is the responsibility of the frontend that sits in front of the
@@ -68,9 +68,8 @@ GoogleAPIKey = "asdfghjkl"
 # matching:
 #   https://example.com/wpk/(.*)
 # to:
-#   http://packager.internal/priv/doc?fetch=http%3A%2F%2Fwww.internal%2F\1&sign=https%3A%2F%2Fexample.com%2F\1
-# where \1 is the URL-escaped value of the regexp group matched above (modulo
-# proper URL escaping).
+#   http://packager.internal/priv/doc?fetch=https%3A%2F%2Fexample.com%2F\1&sign=https%3A%2F%2Fexample.com%2F\1
+# where \1 is the URL-escaped value of the regexp group matched above.
 [[URLSet]]
   # If true, enforces that the path and query are the same between the fetch
   # and sign URLs (e.g. respond in error if
@@ -83,13 +82,11 @@ GoogleAPIKey = "asdfghjkl"
   # be a 200, and must have Cache-Control: public; otherwise, the packager will
   # respond with an error code.
   [URLSet.Fetch]
-    # The set of allowed schemes. Default is ["http", "https"]. You may configure
-    # it to be ["http"] or ["https"] instead. (No other value is allowed.)
+    # The set of allowed schemes. Default is ["https"]. You may configure it to
+    # be ["http"] or ["http", "https"] instead. (No other value is allowed.)
     #
-    # If fetching over http, then the domain should be an internal one (not
-    # routed over the open internet), to avoid a potential for a MITM attack. If
-    # you're unsure of whether your request will be routed internally or not,
-    # consider changing Scheme to ["https"] only.
+    # The scheme is strongly encouraged to be https, to avoid a potential for a
+    # MITM attack.
     Scheme = ["https"]
 
     # The `user:pass@` portion of the URL is disallowed. There is no way to configure this.

bin/amppkg.go
@@ -34,6 +34,9 @@ import (
 
 var flagConfig = flag.String("config", "amppkg.toml", "Path to the config toml file.")
 
+// Prints errors returned by pkg/errors with stack traces.
+func die(err interface{}) { log.Fatalf("%+v", err) }
+
 type logIntercept struct {
 	handler http.Handler
 }
@@ -53,47 +56,47 @@ func main() {
 	flag.Parse()
 	config, err := amppkg.ReadConfig(*flagConfig)
 	if err != nil {
-		panic(errors.Wrap(err, "reading config"))
+		die(errors.Wrap(err, "reading config"))
 	}
 
 	// TODO(twifkak): Document what cert/key storage formats this accepts.
 	certPem, err := ioutil.ReadFile(config.CertFile)
 	if err != nil {
-		panic(errors.Wrapf(err, "reading %s", config.CertFile))
+		die(errors.Wrapf(err, "reading %s", config.CertFile))
 	}
 	keyPem, err := ioutil.ReadFile(config.KeyFile)
 	if err != nil {
-		panic(errors.Wrapf(err, "reading %s", config.KeyFile))
+		die(errors.Wrapf(err, "reading %s", config.KeyFile))
 	}
 
 	certs, err := signedexchange.ParseCertificates(certPem)
 	if err != nil {
-		panic(errors.Wrapf(err, "parsing %s", config.CertFile))
+		die(errors.Wrapf(err, "parsing %s", config.CertFile))
 	}
 	if certs == nil || len(certs) == 0 {
-		panic(fmt.Sprintf("no cert found in %s", config.CertFile))
+		die(fmt.Sprintf("no cert found in %s", config.CertFile))
 	}
 	cert := certs[0]
 	// TODO(twifkak): Verify that cert covers all the signing domains in the config.
 
 	keyBlock, _ := pem.Decode(keyPem)
 	if keyBlock == nil {
-		panic(fmt.Sprintf("no key found in %s", config.KeyFile))
+		die(fmt.Sprintf("no key found in %s", config.KeyFile))
 	}
 
 	key, err := signedexchange.ParsePrivateKey(keyBlock.Bytes)
 	if err != nil {
-		panic(errors.Wrapf(err, "parsing %s", config.KeyFile))
+		die(errors.Wrapf(err, "parsing %s", config.KeyFile))
 	}
 	// TODO(twifkak): Verify that key matches cert.
 
 	packager, err := amppkg.NewPackager(cert, key, config.PackagerBase, config.URLSet)
 	if err != nil {
-		panic(errors.Wrap(err, "building packager"))
+		die(errors.Wrap(err, "building packager"))
 	}
 	certCache, err := amppkg.NewCertCache(cert, certPem)
 	if err != nil {
-		panic(errors.Wrap(err, "building cert cache"))
+		die(errors.Wrap(err, "building cert cache"))
 	}
 
 	// TODO(twifkak): Make log output configurable.

bin/test_cache.go
@@ -30,5 +30,6 @@ func main() {
 		resp.Header().Set("Content-Type", "application/signed-exchange;v=b0")
 		http.ServeFile(resp, req, *flagPackage)
 	})
+	log.Println("Serving on port", *flagPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprint("localhost:", *flagPort), nil))
 }

commit 6ca2af59363a856a100a36b845bf4a762d95b501

Fix tests.
packager_test was broken by commit 3c3f1996, which rewrites the fetch
URL to an AMP CDN URL. certcache_test was broken by commit 16235b62,
which omits base64 padding chars from the URL.

packager.go
@@ -32,6 +32,9 @@ import (
 	"github.com/pquerna/cachecontrol"
 )
 
+// The base URL for transformed fetch URLs.
+var ampCDNBase = "https://cdn.ampproject.org/c/"
+
 // Allowed schemes for the PackagerBase URL, from which certUrls are constructed.
 var acceptablePackagerSchemes = map[string]bool{"http": true, "https": true}
 

certcache_test.go
@@ -31,7 +31,7 @@ func newCertCache(t *testing.T) *CertCache {
 }
 
 func TestServesCertificate(t *testing.T) {
-	resp := get(t, newCertCache(t), "/amppkg/cert/sLtQsuGUOYdCsBVuMTUG_6QBAWFHu8rhEokEHQAWmto=")
+	resp := get(t, newCertCache(t), "/amppkg/cert/sLtQsuGUOYdCsBVuMTUG_6QBAWFHu8rhEokEHQAWmto")
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("invalid status: %#v", resp)
 	}

packager_test.go
@@ -19,6 +19,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -35,21 +36,28 @@ func newPackager(t *testing.T, urlSets []URLSet) *Packager {
 func stringPtr(s string) *string { return &s }
 
 func TestSimple(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("yum yum yum"))
-	}))
-	url, _ := url.Parse(server.URL)
-	defer server.Close()
 	urlSets := []URLSet{URLSet{
 		SamePath: true,
-		Fetch:    URLPattern{[]string{"http"}, url.Host, stringPtr("/amp/.*"), []string{}, stringPtr(""), false},
+		Fetch:    URLPattern{[]string{"http"}, "example.com", stringPtr("/amp/.*"), []string{}, stringPtr(""), false},
 		Sign:     URLPattern{[]string{"https"}, "example.com", stringPtr("/amp/.*"), []string{}, stringPtr(""), false}}}
-	resp := get(t, newPackager(t, urlSets), `/priv/doc?fetch=http%3A%2F%2F`+url.Host+`%2Famp%2Fsecret-life-of-pine-trees.html&sign=https%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html`)
+	resp := get(t, newPackager(t, urlSets), `/priv/doc?fetch=http%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html&sign=https%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html`)
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("incorrect status: %#v", resp)
 	}
 	_, _ = ioutil.ReadAll(resp.Body)
 	// TODO(twifkak): Test the body somehow.
+}
+
+func TestMain(m *testing.M) {
+	// Mock out AMP CDN endpoint.
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("yum yum yum"))
+	}))
+	defer server.Close()
+	url, _ := url.Parse(server.URL)
+	ampCDNBase = url.Host
+
+	os.Exit(m.Run())
 }
 
 // TODO(twifkak): Write lots more tests.