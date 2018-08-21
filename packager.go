ampproject/amppackager

commit 8aefb6153e6cb6d9f5f3fa266a3680ad1f984f91

Proxy redirects unsigned. (#47)
Also, add a test for the 304 logic added in #34, and make sure it always
closes the Body reader.

Fixes #38 and #45.

packager.go
@@ -230,6 +230,10 @@ type Packager struct {
 	urlSets     []URLSet
 }
 
+func noRedirects(req *http.Request, via []*http.Request) error {
+	return http.ErrUseLastResponse
+}
+
 func NewPackager(cert *x509.Certificate, key crypto.PrivateKey, packagerBase string, urlSets []URLSet) (*Packager, error) {
 	baseURL, err := url.Parse(packagerBase)
 	if err != nil {
@@ -247,6 +251,7 @@ func NewPackager(cert *x509.Certificate, key crypto.PrivateKey, packagerBase str
 		return nil, errors.Wrapf(err, "parsing PackagerBase %q with ValidityMapURL %q", packagerBase, ValidityMapURL)
 	}
 	client := http.Client{
+		CheckRedirect: noRedirects,
 		// TODO(twifkak): Load-test and see if default transport settings are okay.
 		Timeout: 60 * time.Second,
 	}
@@ -326,6 +331,23 @@ func (this Packager) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	defer func() {
+		if err := fetchResp.Body.Close(); err != nil {
+			log.Println("Error closing fetchResp body:", err)
+		}
+	}()
+
+	// If fetchURL returns a redirect, then forward that along; do not sign it and do not error out.
+	if fetchResp.StatusCode == 301 || fetchResp.StatusCode == 302 || fetchResp.StatusCode == 303 {
+		resp.Header().Set("location", fetchResp.Header.Get("location"))
+		resp.WriteHeader(fetchResp.StatusCode)
+		_, err := io.Copy(resp, fetchResp.Body)
+		if err != nil {
+			log.Println("Error writing redirect body:", err)
+		}
+		return
+	}
+
 	// If fetchURL returns a 304, then also return a 304 with appropriate headers.
 	if fetchResp.StatusCode == 304 {
 		for header := range statusNotModifiedHeaders {
@@ -334,15 +356,8 @@ func (this Packager) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 			}
 		}
 		resp.WriteHeader(http.StatusNotModified)
-		resp.Write([]byte(""))
 		return
 	}
-
-	defer func() {
-		if err := fetchResp.Body.Close(); err != nil {
-			log.Println("Error closing fetchResp body:", err)
-		}
-	}()
 
 	if httpErr := validateFetch(fetchReq, fetchResp); httpErr != nil {
 		httpErr.LogAndRespond(resp)

packager_test.go
@@ -23,13 +23,26 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/nyaxt/webpackage/go/signedexchange"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
-	"github.com/nyaxt/webpackage/go/signedexchange"
 )
 
 var fakeBody = []byte("They like to OPINE. Get it? (Is he fir real? Yew gotta be kidding me.)")
 var lastRequestURL string
+
+// Don't override this manually; use replacingFakeHandler() instead.
+var fakeHandler = func(w http.ResponseWriter, req *http.Request) {
+	lastRequestURL = req.URL.String()
+	w.Write(fakeBody)
+}
+
+func replacingFakeHandler(newFake func(w http.ResponseWriter, req *http.Request), testCode func()) {
+	oldFake := fakeHandler
+	defer func() { fakeHandler = oldFake }()
+	fakeHandler = newFake
+	testCode()
+}
 
 func newPackager(t *testing.T, urlSets []URLSet) *Packager {
 	handler, err := NewPackager(cert, key, "https://example.com/", urlSets)
@@ -59,13 +72,10 @@ func TestSimple(t *testing.T) {
 		Fetch: &URLPattern{[]string{"http"}, "", "example.com", stringPtr("/amp/.*"), []string{}, stringPtr(""), false, boolPtr(true)},
 	}}
 	resp := get(t, newPackager(t, urlSets), `/priv/doc?fetch=http%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html&sign=https%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html`)
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("incorrect status: %#v", resp)
-	}
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "incorrect status: %#v", resp)
+
 	exchange, err := signedexchange.ReadExchangeFile(resp.Body)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
 	assert.Equal(t, "/example.com/amp/secret-life-of-pine-trees.html?usqp=mq331AQCSAE", lastRequestURL)
 	assert.Equal(t, "https://example.com/amp/secret-life-of-pine-trees.html", exchange.RequestUri.String())
 	assert.Equal(t, http.Header{}, exchange.RequestHeaders)
@@ -80,13 +90,10 @@ func TestNoFetchParam(t *testing.T) {
 		Sign: &URLPattern{[]string{"https"}, "", "example.com", stringPtr("/amp/.*"), []string{}, stringPtr(""), false, nil},
 	}}
 	resp := get(t, newPackager(t, urlSets), `/priv/doc?sign=https%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html`)
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("incorrect status: %#v", resp)
-	}
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "incorrect status: %#v", resp)
+
 	exchange, err := signedexchange.ReadExchangeFile(resp.Body)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
 	assert.Equal(t, "/s/example.com/amp/secret-life-of-pine-trees.html?usqp=mq331AQCSAE", lastRequestURL)
 	assert.Equal(t, "https://example.com/amp/secret-life-of-pine-trees.html", exchange.RequestUri.String())
 }
@@ -97,17 +104,48 @@ func TestErrorNoCache(t *testing.T) {
 	}}
 	// Missing sign param generates an error.
 	resp := get(t, newPackager(t, urlSets), `/priv/doc?fetch=http%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html`)
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("incorrect status: %#v", resp)
-	}
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode, "incorrect status: %#v", resp)
 	assert.Equal(t, "no-store", resp.Header.Get("Cache-Control"))
+}
+
+func TestRedirectIsProxiedUnsigned(t *testing.T) {
+	urlSets := []URLSet{URLSet{
+		Sign: &URLPattern{[]string{"https"}, "", "example.com", stringPtr("/amp/.*"), []string{}, stringPtr(""), false, nil},
+	}}
+	replacingFakeHandler(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("cookie", "yum yum yum")
+		w.Header().Set("location", "https://example.com/login")
+		w.WriteHeader(301)
+	}, func() {
+		resp := get(t, newPackager(t, urlSets), `/priv/doc?sign=https%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html`)
+		assert.Equal(t, 301, resp.StatusCode)
+		assert.Equal(t, "", resp.Header.Get("cookie"))
+		assert.Equal(t, "https://example.com/login", resp.Header.Get("location"))
+	})
+}
+
+func TestNotModifiedIsProxiedUnsigned(t *testing.T) {
+	urlSets := []URLSet{URLSet{
+		Sign: &URLPattern{[]string{"https"}, "", "example.com", stringPtr("/amp/.*"), []string{}, stringPtr(""), false, nil},
+	}}
+	replacingFakeHandler(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("cache-control", "private")
+		w.Header().Set("cookie", "yum yum yum")
+		w.Header().Set("etag", "superrad")
+		w.WriteHeader(304)
+	}, func() {
+		resp := get(t, newPackager(t, urlSets), `/priv/doc?sign=https%3A%2F%2Fexample.com%2Famp%2Fsecret-life-of-pine-trees.html`)
+		assert.Equal(t, 304, resp.StatusCode)
+		assert.Equal(t, "private", resp.Header.Get("cache-control"))
+		assert.Equal(t, "", resp.Header.Get("cookie"))
+		assert.Equal(t, "superrad", resp.Header.Get("etag"))
+	})
 }
 
 func TestMain(m *testing.M) {
 	// Mock out AMP CDN endpoint.
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		lastRequestURL = req.URL.String()
-		w.Write(fakeBody)
+		fakeHandler(w, req)
 	}))
 	defer server.Close()
 	url, _ := url.Parse(server.URL)

commit 27f22f744d4c7ba730f06475c5f113317b642f6e

Don't rewrite fetch URLs to point to the AMP CDN.
This was used as an interim solution for transforming AMPHTML, but the
real solution is in progress in pkg/transform, and this was broken
anyway in #47 (8aefb615).

packager.go
@@ -33,9 +33,6 @@ import (
 	"github.com/pquerna/cachecontrol"
 )
 
-// The base URL for transformed fetch URLs.
-var AmpCDNBase = "https://cdn.ampproject.org/c/"
-
 // Allowed schemes for the PackagerBase URL, from which certUrls are constructed.
 var acceptablePackagerSchemes = map[string]bool{"http": true, "https": true}
 
@@ -259,24 +256,12 @@ func NewPackager(cert *x509.Certificate, key crypto.PrivateKey, packagerBase str
 	return &Packager{cert, key, validityURL, &client, baseURL, urlSets}, nil
 }
 
-func (this Packager) fetchURL(orig *url.URL, serveHTTPReq http.Header) (*http.Request, *http.Response, *HTTPError) {
-	// Make a copy so destructive changes don't persist.
-	fetch := *orig
-	// Add the query parameter to enable web package transforms.
-	query := fetch.Query()
-	query.Add("usqp", "mq331AQCSAE")
-	fetch.RawQuery = query.Encode()
-
-	ampURL := AmpCDNBase
-	if fetch.Scheme == "https" {
-		ampURL += "s/"
-	}
-	ampURL += fetch.Host + fetch.RequestURI()
+func (this Packager) fetchURL(fetch *url.URL, serveHTTPReq http.Header) (*http.Request, *http.Response, *HTTPError) {
+	ampURL := fetch.String()
 
 	log.Printf("Fetching URL: %q\n", ampURL)
-	// TODO(twifkak): Translate into AMP CDN URL, until transform API is available.
 	req, err := http.NewRequest(http.MethodGet, ampURL, nil)
-	// TODO(twifkak): Should we add 'Accept-Charset: utf-8'? The AMP Transformer API requires utf-8.
+	// TODO(twifkak): Should we add 'Accept-Charset: utf-8'? Do AMP Caches require it? Will it break more servers than it fixes?
 	if err != nil {
 		return nil, nil, NewHTTPError(http.StatusInternalServerError, "Error building request: ", err)
 	}