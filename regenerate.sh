53ab7b4b35e2fc47fa46a4f79e2477c50e681008
Angie Lin <angielin@google.com>
2018-08-30 11:20:21 -0700

    PiperOrigin-RevId: 210946135

---
+++ regenerate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Regenerate compiled go protos.
+#
+# Requires protoc and protoc-gen-go to be installed.
+# For more info on protoc:
+#    https://developers.google.com/protocol-buffers/
+#
+# To install protoc-gen-go:
+#    go get -u github.com/golang/protobuf/protoc-gen-go
+
+set -e
+
+protoc --go_out=paths=source_relative:. pkg/transform/request/request.proto
