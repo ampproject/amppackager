9906a3a2daec6224400721f8b41059af1cdd14fc
Devin Mullins <twifkak@google.com>
2018-10-03 13:58:42 -0700

    Add test for validitymap.go.

---
+++ packager/validitymap/validitymap_test.go
@@ -0,0 +1,24 @@
+package validitymap
+
+import (
+	"io/ioutil"
+	"testing"
+
+	pkgt "github.com/ampproject/amppackager/packager/testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidityMap(t *testing.T) {
+	handler, err := New()
+	require.NoError(t, err)
+
+	resp := pkgt.Get(t, handler, "/")
+	defer resp.Body.Close()
+	assert.Equal(t, "application/cbor", resp.Header.Get("Content-Type"))
+	assert.Equal(t, "public, max-age=604800", resp.Header.Get("Cache-Control"))
+
+	body, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+	assert.Equal(t, []byte("\xA0"), body)
+}
