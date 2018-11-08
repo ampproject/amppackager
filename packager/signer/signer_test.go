5d36b091eb9469284aff1b164508dd97cc0758d9
Devin Mullins <twifkak@google.com>
2018-11-07 19:01:42 -0800

    Percent-escape URL-invalid query chars in Link.
    This fixes preloads for fonts.googleapis.com, which include `|` in their
    query component.

--- packager/signer/signer_test.go
+++ packager/signer/signer_test.go
@@ -293,14 +293,14 @@ func (this *SignerSuite) TestEscapesLinkHeaders() {
 		// However, it would be nice to limit the impact that could be
 		// caused by transformation of an invalid AMP, e.g. on a
 		// same-origin impression.
-		resp.Write([]byte(`<html amp><head><script src="https://foo.com/a,b>c">`))
+		resp.Write([]byte(`<html amp><head><script src="https://foo.com/a,b>c?d>e|f">`))
 	}
 	resp := this.get(this.T(), this.new(urlSets), "/priv/doc?sign="+url.QueryEscape(this.httpsURL()+fakePath))
 	this.Assert().Equal(http.StatusOK, resp.StatusCode, "incorrect status: %#v", resp)
 
 	exchange, err := signedexchange.ReadExchange(resp.Body)
 	this.Require().NoError(err)
-	this.Assert().Equal("<https://foo.com/a,b%3Ec>;rel=preload;as=script", exchange.ResponseHeaders.Get("Link"))
+	this.Assert().Equal("<https://foo.com/a,b%3Ec?d%3Ee%7Cf>;rel=preload;as=script", exchange.ResponseHeaders.Get("Link"))
 }
 
 func (this *SignerSuite) TestErrorNoCache() {
