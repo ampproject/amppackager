6c6cf965c99c213b09c2a81c056a89cddef05cb7
Devin Mullins <twifkak@google.com>
2018-12-12 22:05:02 -0800

    Add go1.11 build tag.
    This excludes it from the Travis build, which is on go1.10 (since that is
    our current supported version).

--- cmd/transform_wasm/main.go
+++ cmd/transform_wasm/main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build go1.11
+
 /*
 Binary wrapper for the transformer lib, to be run via main.js.
 */
