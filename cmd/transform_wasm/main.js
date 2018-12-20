00e4646faac7b5342c7dc46de2ae266f0cf71d9d
Devin Mullins <twifkak@google.com>
2018-12-19 19:58:20 -0800

    Reduce Go-WASM memory leak with TypedArrays.
    1. Reuse Uint8Arrays to communicate with the Go process, since
    inputs/outputs are never garbage-collected.
    
    I believe this still has a tiny memory leak of the lengths passed back
    and forth. This should be fixed by encoding the length into the
    Uint8Array as well (either as a prefix or a NUL terminator).
    
    2. Use https://github.com/termonio/wams to shrink the Go heap from 1G
    to 768M. Shrinking it any more causes:
    
    TypeError: Cannot perform %TypedArray%.prototype.set on a detached ArrayBuffer
    
    Perhaps under a memory crunch, the Go runtime moves things around to
    defragment? I'm not really sure.
    
    3. Add some skeletal support for Go 1.12, which will be changing the
    JS->Go function call API.

--- cmd/transform_wasm/main.js
+++ cmd/transform_wasm/main.js
@@ -15,16 +15,20 @@
 // Bootstrapper for transform_wasm. Transforms all html in the test files
 // specified on the commandline.
 //
+// Build dependency: go get github.com/termonio/wams
+//
 // To use:
 //   GOOS=js GOARCH=wasm go build -o transform.wasm ./cmd/transform_wasm/ &&
-//   node --max-old-space-size=7000 cmd/transform_wasm/main.js transform.wasm \
+//   wams -pages 12288 -write transform.wasm &&
+//   node --max-old-space-size=4000 cmd/transform_wasm/main.js transform.wasm \
 //     path/to/test/files*
 
-// TODO(twifkak): Investigate slowdown over time.
-// TODO(twifkak): Investigate memory usage growth.
+// TODO(twifkak): Figure out how to reduce Go heap size from 768M.
+// TODO(twifkak): Pass lengths via typedarrays as well.
 
-const assert = require('assert');
 const fs = require('fs');
+const util = require('util');
+
 const { join } = require('path');
 const { spawnSync } = require('child_process');
 
@@ -63,9 +67,10 @@ async function readTestFiles() {
   }
 
   // Parse the URL from each test case.
+  const encoder = new util.TextEncoder();
   htmls.forEach((html, i) => {
     let newline = html.indexOf('\n');
-    htmls[i] = [html.substring(0, newline), html.substring(newline + 1)];
+    htmls[i] = [encoder.encode(html.substring(0, newline)), encoder.encode(html.substring(newline + 1))];
   });
 
   console.log('Pushed all %d tests.', htmls.length);
@@ -73,23 +78,56 @@ async function readTestFiles() {
   return htmls;
 }
 
-global.begin = async function(transform, done) {
+function dumpHeap(name) {
+  console.log('%s: %o', name, process.memoryUsage());
+}
+
+// Copies inp plus NUL-terminator into out.
+global.begin = async function(transform, done, urlIn, htmlIn, htmlOut, maxLen) {
+  dumpHeap('compile.after');
   let num = 0;
-  let outs = (await readTestFiles()).map(([url, html]) =>
-    new Promise((resolve) =>
-      transform(url, html, (amphtml) => {
-          if (++num % 100 == 0) console.log('num = ', num);
-          // Minimum valid AMP is larger than 1K.
-          if (amphtml.length < 1000) console.log('URL ', url, ' output is invalid: ', amphtml);
-          resolve(amphtml);
-      })));
-  console.log('Pushed all %d thunks.', outs.length);
+  const decoder = new util.TextDecoder();
+  dumpHeap('transform.before');
   const start = process.hrtime.bigint();
-  await Promise.all(outs);
+  for (const [url, html] of tests) {
+    if (++num % 100 === 0) console.log('num = ', num);
+    if (num % 2000 === 0) dumpHeap('transform.' + num);
+    if (url.length > 2000) {
+      console.log("url too big: ", decoder.decode(url));
+      continue;
+    }
+    if (html.length > maxLen) {
+      console.log("html too big (", html.length, ") for url: ", decoder.decode(url));
+      continue;
+    }
+    urlIn.set(url);
+    htmlIn.set(html);
+
+    if (true /* go1.11 */) {
+      await new Promise((resolve) =>
+        transform(url.length, html.length, (htmlOutLen) => {
+          // Minimum valid AMP is larger than 1K.
+          if (htmlOutLen < 1000) console.log('URL ', decoder.decode(url), ' output is invalid: ', decoder.decode(htmlOut.slice(htmlOutLen)));
+          resolve();
+        }));
+    } else /* go1.12 */ {
+      const htmlOutLen = transform(url.length, html.length);
+      // Minimum valid AMP is larger than 1K.
+      if (htmlOutLen < 1000) console.log('URL ', decoder.decode(url), ' output is invalid: ', decoder.decode(htmlOut.slice(htmlOutLen)));
+    }
+  }
   const total = process.hrtime.bigint() - start;
-  console.log(`Took ${total} nanoseconds, or ${Number(total) / outs.length / 1000000} millis per doc.`);
+  dumpHeap('transform.after');
+  console.log(`Took ${total} nanoseconds, or ${Number(total) / tests.length / 1000000} millis per doc.`);
   done();
 }
 
-const goroot = process.env.GOROOT || spawnSync('go', ['env', 'GOROOT']).stdout.toString().trim();
-require(join(goroot, 'misc/wasm/wasm_exec.js'));
+async function main() {
+  global.tests = await readTestFiles();
+
+  dumpHeap('compile.before');
+  const goroot = process.env.GOROOT || spawnSync('go', ['env', 'GOROOT']).stdout.toString().trim();
+  require(join(goroot, 'misc/wasm/wasm_exec.js'));
+}
+
+main();
