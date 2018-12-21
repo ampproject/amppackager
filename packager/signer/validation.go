ampproject/amppackager

commit 2ea2f2dfbb59bcafba4dbcc78b45d877bf776cb0

Add a 2000-char URL length limit by default. (#214)

amppkg.example.toml
@@ -84,8 +84,18 @@ OCSPCache = '/tmp/amppkg-ocsp'
     # certificate must cover this domain.
     Domain = "amppackageexample.com"
 
-    # A full-match regexp on the path (not including the ?query).
-    # Defaults to ".*". The below example allows paths starting with /world/.
+    # A full-match regexp on the path (not including the ?query). Defaults to
+    # ".{,2000}" -- that is, any path <= 2000 chars long.
+    #
+    # The length limit is to mitigate the risk of a content-sniffing attack
+    # (per https://github.com/WICG/webpackage/pull/348) against downstream
+    # servers that serve 200 responses regardless of some of the path
+    # components (e.g. looking up by id and not validating the slug in the
+    # requested URL). If your server only serves 200 responses on a limited set
+    # of valid URLs, and some of those URLs are >2000 chars, then it's safe to
+    # modify this default.
+    #
+    # The below example allows paths starting with /world/.
     # PathRE = "/world/.*"
 
     # A list of full-match regexps, carving out exclusions to the above PathRE.

packager/util/config.go
@@ -49,12 +49,12 @@ type URLPattern struct {
 }
 
 var emptyRegexp = ""
-var dotStarRegexp = ".*"
+var defaultPathRegexp = ".{,2000}"
 
 // Also sets defaults.
 func validateURLPattern(pattern *URLPattern) error {
 	if pattern.PathRE == nil {
-		pattern.PathRE = &dotStarRegexp
+		pattern.PathRE = &defaultPathRegexp
 	} else if _, err := regexp.Compile(*pattern.PathRE); err != nil {
 		return errors.New("PathRE must be a valid regexp")
 	}

packager/util/config_test.go
@@ -43,7 +43,7 @@ func TestMinimalValidConfig(t *testing.T) {
 		URLSet: []URLSet{{
 			Sign: &URLPattern{
 				Domain:  "example.com",
-				PathRE:  stringPtr(".*"),
+				PathRE:  stringPtr(".{,2000}"),
 				QueryRE: stringPtr(""),
 			},
 		}},
@@ -186,7 +186,7 @@ func TestFetchDefaults(t *testing.T) {
 	assert.ElementsMatch(t, []string{"http", "https"}, fetch.Scheme)
 	assert.Equal(t, "", fetch.DomainRE)
 	assert.Equal(t, "example.com", fetch.Domain)
-	assert.Equal(t, stringPtr(".*"), fetch.PathRE)
+	assert.Equal(t, stringPtr(".{,2000}"), fetch.PathRE)
 	assert.Nil(t, fetch.PathExcludeRE)
 	assert.Equal(t, stringPtr(""), fetch.QueryRE)
 	assert.Equal(t, false, fetch.ErrorOnStatefulHeaders)

commit f618a6d577e1b601278b6d52c581175d45702750

Fix faulty PathRE and create MaxLength.
Move the 2000-char limit from the PathRE into a separate MaxLength
config field. The regex was not valid syntax per
https://github.com/google/re2/wiki/Syntax. This was broken in commit
2ea2f2df (#214).

amppkg.example.toml
@@ -85,15 +85,7 @@ OCSPCache = '/tmp/amppkg-ocsp'
     Domain = "amppackageexample.com"
 
     # A full-match regexp on the path (not including the ?query). Defaults to
-    # ".{,2000}" -- that is, any path <= 2000 chars long.
-    #
-    # The length limit is to mitigate the risk of a content-sniffing attack
-    # (per https://github.com/WICG/webpackage/pull/348) against downstream
-    # servers that serve 200 responses regardless of some of the path
-    # components (e.g. looking up by id and not validating the slug in the
-    # requested URL). If your server only serves 200 responses on a limited set
-    # of valid URLs, and some of those URLs are >2000 chars, then it's safe to
-    # modify this default.
+    # ".*".
     #
     # The below example allows paths starting with /world/.
     # PathRE = "/world/.*"
@@ -122,6 +114,16 @@ OCSPCache = '/tmp/amppkg-ocsp'
     # are stripped from the response before packaging. If instead you wish for
     # this to cause packaging to fail, set ErrorOnStatefulHeaders = true.
     # ErrorOnStatefulHeaders = true
+
+    # The maximum length of the URL. Defaults to 2000 as this is a de facto
+    # limit anyway in many browsers. The limit is to mitigate the risk of a
+    # content-sniffing attack (per https://github.com/WICG/webpackage/pull/348)
+    # against downstream servers that serve 200 responses regardless of some of
+    # the path components (e.g. looking up by id and not validating the slug in
+    # the requested URL). If your server only serves 200 responses on a limited
+    # set of valid URLs, and some of those URLs are >2000 chars, then it's safe
+    # to modify this default.
+    # MaxLength = 2000
 
   # By default, the packager only looks at the sign param, and fetches the
   # content from the same location. If you'd like more flexibility (for

packager/signer/validation.go
@@ -79,6 +79,9 @@ func urlMatches(url *url.URL, pattern util.URLPattern) error {
 	if !regexpFullMatch(*pattern.QueryRE, url.RawQuery) {
 		return errors.New("QueryRE doesn't match")
 	}
+	if len(url.String()) > pattern.MaxLength {
+		return errors.New("URL too long")
+	}
 	return nil
 }
 

packager/util/config.go
@@ -45,11 +45,12 @@ type URLPattern struct {
 	PathExcludeRE          []string
 	QueryRE                *string
 	ErrorOnStatefulHeaders bool
+	MaxLength              int
 	SamePath               *bool
 }
 
 var emptyRegexp = ""
-var defaultPathRegexp = ".{,2000}"
+var defaultPathRegexp = ".*"
 
 // Also sets defaults.
 func validateURLPattern(pattern *URLPattern) error {
@@ -67,6 +68,9 @@ func validateURLPattern(pattern *URLPattern) error {
 		pattern.QueryRE = &emptyRegexp
 	} else if _, err := regexp.Compile(*pattern.QueryRE); err != nil {
 		return errors.New("QueryRE must be a valid regexp")
+	}
+	if pattern.MaxLength == 0 {
+		pattern.MaxLength = 2000
 	}
 	return nil
 }

packager/util/config_test.go
@@ -43,7 +43,7 @@ func TestMinimalValidConfig(t *testing.T) {
 		URLSet: []URLSet{{
 			Sign: &URLPattern{
 				Domain:  "example.com",
-				PathRE:  stringPtr(".{,2000}"),
+				PathRE:  stringPtr(".*"),
 				QueryRE: stringPtr(""),
 			},
 		}},
@@ -186,7 +186,7 @@ func TestFetchDefaults(t *testing.T) {
 	assert.ElementsMatch(t, []string{"http", "https"}, fetch.Scheme)
 	assert.Equal(t, "", fetch.DomainRE)
 	assert.Equal(t, "example.com", fetch.Domain)
-	assert.Equal(t, stringPtr(".{,2000}"), fetch.PathRE)
+	assert.Equal(t, stringPtr(".*"), fetch.PathRE)
 	assert.Nil(t, fetch.PathExcludeRE)
 	assert.Equal(t, stringPtr(""), fetch.QueryRE)
 	assert.Equal(t, false, fetch.ErrorOnStatefulHeaders)