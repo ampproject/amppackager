328f2d03e12e7cb20ce0c723f97d30f9af89ac38
Angie Lin <angielin@google.com>
2019-04-09 15:46:14 -0700

    Use t.Run to clean up tests and save printing tc.desc everywhere manually. Also standardize on 'tc' and 'tcs' for variable names.
    Tackling one directory, or subset of tests at a time.
    
    PiperOrigin-RevId: 242755445

--- transformer/versions_test.go
+++ transformer/versions_test.go
@@ -7,134 +7,156 @@ import (
 )
 
 func TestValidateRequest(t *testing.T) {
-	tests := []struct {
-		rs []*rpb.VersionRange
-		expectedError        bool
+	tcs := []struct {
+		desc          string
+		rs            []*rpb.VersionRange
+		expectedError bool
 	}{
 		{
+			desc:          "nil",
 			rs:            nil,
 			expectedError: false,
 		},
 		{
+			desc:          "empty",
 			rs:            []*rpb.VersionRange{},
 			expectedError: false,
 		},
 		{
+			desc:          "malformed",
 			rs:            []*rpb.VersionRange{{Max: 1, Min: 2}},
-			expectedError: true, // Malformed.
+			expectedError: true,
 		},
 		{
+			desc:          "negative",
 			rs:            []*rpb.VersionRange{{Max: 1, Min: -1}},
-			expectedError: true, // Negative.
+			expectedError: true,
 		},
 		{
+			desc:          "same",
 			rs:            []*rpb.VersionRange{{Max: 1, Min: 1}},
 			expectedError: false,
 		},
 		{
+			desc:          "overlapping",
 			rs:            []*rpb.VersionRange{{Max: 2, Min: 1}, {Max: 1, Min: 1}},
-			expectedError: true, // Overlapping.
+			expectedError: true,
 		},
 		{
+			desc:          "valid",
 			rs:            []*rpb.VersionRange{{Max: 3, Min: 2}, {Max: 1, Min: 1}},
 			expectedError: false,
 		},
 		{
+			desc:          "out of order",
 			rs:            []*rpb.VersionRange{{Max: 1, Min: 1}, {Max: 3, Min: 2}},
-			expectedError: true, // Out of order.
+			expectedError: true,
 		},
 	}
-	for _, test := range tests {
-		err := validateRequest(test.rs)
-		if test.expectedError != (err != nil) {
-			t.Errorf("validateRequest(%+v) unexpected err = %v", test.rs, err)
-		}
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := validateRequest(tc.rs)
+			if tc.expectedError != (err != nil) {
+				t.Errorf("validateRequest(%+v) unexpected err = %v", tc.rs, err)
+			}
+		})
 	}
 }
 
 func TestSelectVersion(t *testing.T) {
-	tests := []struct {
+	tcs := []struct {
+		desc                 string
 		requested, supported []*rpb.VersionRange
 		expectedVersion      int64
 		expectedError        bool
 	}{
-		// No supported.
 		{
+			desc:          "not supported",
 			requested:     []*rpb.VersionRange{{Max: 4, Min: 1}},
 			supported:     []*rpb.VersionRange{},
 			expectedError: true,
 		},
-		// No request, highest version supported.
 		{
+			desc:            "No request, highest version supported.",
 			requested:       []*rpb.VersionRange{},
 			supported:       []*rpb.VersionRange{{Max: 4, Min: 1}},
 			expectedVersion: 4,
 		},
-		// No request (nil), highest version supported.
 		{
+			desc:            "No request (nil), highest version supported.",
 			requested:       nil,
 			supported:       []*rpb.VersionRange{{Max: 4, Min: 1}},
 			expectedVersion: 4,
 		},
-		// One version, match.
 		{
+			desc:            "One version, match.",
 			requested:       []*rpb.VersionRange{{Max: 1, Min: 1}},
 			supported:       []*rpb.VersionRange{{Max: 1, Min: 1}},
 			expectedVersion: 1,
 		},
-		// One version, mismatch.
+
 		{
+			desc:          "One version, mismatch.",
 			requested:     []*rpb.VersionRange{{Max: 1, Min: 1}},
 			supported:     []*rpb.VersionRange{{Max: 2, Min: 2}},
 			expectedError: true,
 		},
-		// Supported is a subset of requested.
+
 		{
+			desc:            "Supported is a subset of requested.",
 			requested:       []*rpb.VersionRange{{Max: 4, Min: 1}},
 			supported:       []*rpb.VersionRange{{Max: 2, Min: 2}},
 			expectedVersion: 2,
 		},
-		// Requested is a subset of supported.
+
 		{
+			desc:            "Requested is a subset of supported.",
 			requested:       []*rpb.VersionRange{{Max: 2, Min: 2}},
 			supported:       []*rpb.VersionRange{{Max: 4, Min: 1}},
 			expectedVersion: 2,
 		},
-		// Requested and supported abut.
+
 		{
+			desc:            "Requested and supported abut.",
 			requested:       []*rpb.VersionRange{{Max: 4, Min: 2}},
 			supported:       []*rpb.VersionRange{{Max: 2, Min: 1}},
 			expectedVersion: 2,
 		},
-		// Requested includes a hole in supported.
+
 		{
+			desc:            "Requested includes a hole in supported.",
 			requested:       []*rpb.VersionRange{{Max: 2, Min: 1}},
 			supported:       []*rpb.VersionRange{{Max: 4, Min: 3}, {Max: 1, Min: 1}},
 			expectedVersion: 1,
 		},
-		// Requested requires a hole in supported.
+
 		{
+			desc:          "Requested requires a hole in supported.",
 			requested:     []*rpb.VersionRange{{Max: 2, Min: 2}},
 			supported:     []*rpb.VersionRange{{Max: 4, Min: 3}, {Max: 1, Min: 1}},
 			expectedError: true,
 		},
-		// Overlap is between the second elements of both requested and supported.
+
 		{
+			desc:            "Overlap is between the second elements of both requested and supported.",
 			requested:       []*rpb.VersionRange{{Max: 6, Min: 5}, {Max: 2, Min: 1}},
 			supported:       []*rpb.VersionRange{{Max: 4, Min: 3}, {Max: 1, Min: 1}},
 			expectedVersion: 1,
 		},
 	}
-	for _, test := range tests {
-		v, err := selectVersion(test.requested, test.supported)
-		if test.expectedError {
-			if err == nil {
-				t.Errorf("selectVersion(%+v, %+v), want err, got v = %d", test.requested, test.supported, v)
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			v, err := selectVersion(tc.requested, tc.supported)
+			t.Logf("selectVersion(%+v, %+v) = %d", tc.requested, tc.supported, v)
+			if tc.expectedError {
+				if err == nil {
+					t.Error("wanted, but didn't get err")
+				}
+			} else if err != nil {
+				t.Errorf("got unexpected err = %v", err)
+			} else if v != tc.expectedVersion {
+				t.Errorf("retval is unexpectedly %v", v)
 			}
-		} else if err != nil {
-			t.Errorf("selectVersion(%+v, %+v), want v = %d, got err = %v", test.requested, test.supported, test.expectedVersion, err)
-		} else if v != test.expectedVersion {
-			t.Errorf("selectVersion(%+v, %+v), want v = %d, got v = %v", test.requested, test.supported, test.expectedVersion, v)
-		}
+		})
 	}
 }
