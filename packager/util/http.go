f3e374daa85a7cf788c2679526e987ab47dbff60
Matt Mower <matt@wompmobile.com>
2019-06-04 14:03:51 -0700

    accept: Improve tokenization (#296)
    - Support parameter values with commas (inside quotes)
    - Add a few relevant tests for new support
    
    Uses a tokenization algorithm similar to that of spring-projects, which is also
    under the Apache 2.0 license:
      https://github.com/spring-projects/spring-framework/

--- packager/util/http.go
+++ packager/util/http.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"regexp"
 	"net/http"
+	"strings"
 )
 
 // A comma, as defined in https://tools.ietf.org/html/rfc7230#section-7, with
 // OWS defined in https://tools.ietf.org/html/rfc7230#appendix-B. This is
 // commonly used as a separator in header field value definitions.
 var Comma *regexp.Regexp = regexp.MustCompile(`[ \t]*,[ \t]*`)
+
+// Trim optional whitespace from a header value, adhering to
+// https://tools.ietf.org/html/rfc7230#section-7 with OWS defined in
+// https://tools.ietf.org/html/rfc7230#appendix-B.
+func TrimHeaderValue(s string) string {
+	return strings.TrimFunc(s, func(r rune) bool {
+		return r == ' ' || r == '\t'
+	})
+}
 
 // Conditional request headers that ServeHTTP may receive and need to be sent with fetchURL.
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Conditional_requests#Conditional_headers
