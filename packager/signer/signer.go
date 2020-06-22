1c490bae4082ccc84c99d183c7c2b67e86120954
michaelrybak <michaelrybak@google.com>
2020-06-22 16:50:28 -0700

    '

--- packager/signer/signer.go
+++ packager/signer/signer.go
@@ -477,9 +477,9 @@ func (this *Signer) serveSignedExchange(resp http.ResponseWriter, fetchResp *htt
 
 	// If the cap has been applied, it means the document is too large. Signer
 	// won't load all the body into memory, and therefore won't sign the
-	// document. But signer can still proxy the body unchanged: first send the
-	// capped part of the body that signer has already read, and then stream the
-	// rest of the body.
+	// document. But signer can still proxy the document unsigned: first send
+	// the capped part of the body that signer has already read, and then stream
+	// the rest of the body.
 	if len(fetchBody) == maxBodyLength {
 		resp.Write(fetchBody)
 		proxy(resp, fetchResp, nil)
