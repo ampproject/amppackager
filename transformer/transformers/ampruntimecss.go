123d24a3621a7ccb3038e131a9271b0ba6de8e8f
Devin Mullins <twifkak@google.com>
2021-04-16 12:26:33 -0700

    If there is an existing <style amp-runtime>, don't delete it unless we have a suitable replacement. This enables ESM-optimized AMP to be styled correctly in AMP Packager, as the mjs runtime does not include a fallback copy of the CSS.
    PiperOrigin-RevId: 368897273

--- transformer/transformers/ampruntimecss.go
+++ transformer/transformers/ampruntimecss.go
@@ -23,30 +23,56 @@ import (
 	"golang.org/x/net/html"
 )
 
+const versionAttr string = "i-amphtml-version"
+const prodPrefix string = "01"
+
 // AMPRuntimeCSS inlines the contents of the AMP HTML CSS RTV.
+//
+// If there is no style, a new one is added. RTV and CSS are set if available.
+//
+// If there are one or more styles already (e.g. from Optimizer), then they are
+// all deleted and replaced with one iff both RTV and CSS are available and newer.
 func AMPRuntimeCSS(e *Context) error {
-	// If the document already has <style amp-runtime> then remove them. This
-	// can happen if this transformer is run on already transformed AMP.
+	rtv := e.Request.GetRtv()
+	css := e.Request.GetCss()
+
+	nodesToRemove := []*html.Node{}
 	for c := e.DOM.HeadNode.FirstChild; c != nil; c = htmlnode.Next(c) {
 		if c.DataAtom == atom.Style &&
 			htmlnode.HasAttribute(c, "", amphtml.AMPRuntime) {
-			htmlnode.RemoveNode(&c)
+			cRTV, cHasRTV := htmlnode.GetAttributeVal(c, "", versionAttr)
+			// Replace if older than the one in the packager, or if
+			// the RTV is not of the prod variety.
+			if !cHasRTV || !strings.HasPrefix(cRTV, prodPrefix) ||
+			   (rtv != "" && css != "" && rtv > cRTV) {
+				nodesToRemove = append(nodesToRemove, c)
+			} else {
+				// If any styles are not going to be removed,
+				// then bail. We don't want to create a
+				// conflict between existing and new styles.
+				return nil
+			}
 		}
 	}
+
+	// Delete previous styles, if any.
+	for _, n := range nodesToRemove {
+		htmlnode.RemoveNode(&n)
+	}
+
 	// Create <style amp-runtime> tag.
 	n := htmlnode.Element("style", html.Attribute{Key: "amp-runtime"})
 	// Annotate it with the AMP Runtime version that is being inlined.
-	rtv := "latest"
-	if e.Request.GetRtv() != "" {
-		rtv = e.Request.GetRtv()
+	if rtv == "" {
+		rtv = "latest"
 	}
-	htmlnode.SetAttribute(n, "", "i-amphtml-version", rtv)
+	htmlnode.SetAttribute(n, "", versionAttr, rtv)
 	// Place it first in the document <head>.
 	e.DOM.HeadNode.InsertBefore(n, e.DOM.HeadNode.FirstChild)
 
-	// The contents of the runtime css are available, so inline it.
-	if e.Request.GetCss() != "" {
-		n.AppendChild(htmlnode.Text(strings.TrimSpace(e.Request.GetCss())))
+	// If the contents of the runtime css are available, inline it.
+	if css != "" {
+		n.AppendChild(htmlnode.Text(strings.TrimSpace(css)))
 		return nil
 	}
 
